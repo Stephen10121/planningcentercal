2aace8ce837273c5aaa3e82031a550ee95f81939
Stephen Gruzin <stephengrzn@gmail.com>
2025-03-10 15:38:13 -0700

    Changed the color of the tabs depending on the theme preferences.

--- src/lib/ChangeThemeParams.svelte
+++ src/lib/ChangeThemeParams.svelte
@@ -13,7 +13,7 @@
 
 <div>
     <Label>Theme</Label>
-    <div class="tabs">
+    <div class="tabs bg-[#f4f4f5] dark:bg-[#27272a]">
         <label title="Choose Theme"><input form="updateCalendar" type="radio" name="style" class="sr-only" value="LIGHT" bind:group={theme} checked={theme=="LIGHT"} /><p>Light</p></label>
         <label title="Choose Theme"><input form="updateCalendar" type="radio" name="style" class="sr-only" value="DARK" bind:group={theme} checked={theme=="DARK"} /><p>Dark</p></label>
         <label title="Choose Theme"><input form="updateCalendar" type="radio" name="style" class="sr-only" value="CUSTOM" bind:group={theme} checked={theme=="CUSTOM"} /><p>Custom</p></label>
@@ -21,133 +21,133 @@
 </div>
 {#if theme === "CUSTOM"}
     <LoadTemplate bind:customThemeJSON={customThemeJSON} />
+    <Accordion.Root class="w-full" disabled={theme !== "CUSTOM"}>
+        <Accordion.Item value="mainpagestyles">
+            <Accordion.Trigger style="{theme !== "CUSTOM" ? 'opacity: 0.5;':''}">Main Page Styles</Accordion.Trigger>
+            <Accordion.Content>
+                <div class="customisers">
+                    <label class="colorPicker">
+                        <Label>Background Color</Label>
+                        <input type="color" bind:value={customThemeJSON.backgroundColor}>
+                    </label>
+                </div>
+            </Accordion.Content>
+        </Accordion.Item>
+        <Accordion.Item value="headerstyles">
+            <Accordion.Trigger style="{theme !== "CUSTOM" ? 'opacity: 0.5;':''}">Header Styles</Accordion.Trigger>
+            <Accordion.Content>
+                <div class="customisers">
+                    <label class="colorPicker">
+                        <Label>Header Background Color</Label>
+                        <input type="color" bind:value={customThemeJSON.headerColor}>
+                    </label>
+                    <div class="bar"></div>
+                    <label class="colorPicker">
+                        <Label>Day Text Color</Label>
+                        <input type="color" bind:value={customThemeJSON.dayTextColor}>
+                    </label>
+                    <div class="bar"></div>
+                    <label class="colorPicker">
+                        <Label>Selected Day Text Color</Label>
+                        <input type="color" bind:value={customThemeJSON.selectedDayTextColor}>
+                    </label>
+                    <div class="bar"></div>
+                    <label class="text">
+                        <Label>Day of the Week Text Size</Label>
+                        <Input type="text" autocomplete="off" bind:value={customThemeJSON.dayOfWeekSize} />
+                    </label>
+                    <div class="bar"></div>
+                    <label class="text">
+                        <Label>Header Height</Label>
+                        <Input type="text" autocomplete="off" bind:value={customThemeJSON.headerHeight} />
+                    </label>
+                    <div class="bar"></div>
+                    <label class="text">
+                        <Label>Header Shadow</Label>
+                        <Input type="text" autocomplete="off" bind:value={customThemeJSON.headerBoxShadow} />
+                    </label>
+                </div>
+            </Accordion.Content>
+        </Accordion.Item>
+        <Accordion.Item value="dayseparatorstyles">
+            <Accordion.Trigger style="{theme !== "CUSTOM" ? 'opacity: 0.5;':''}">Day Separator Styles</Accordion.Trigger>
+            <Accordion.Content>
+                <div class="customisers">
+                    <label class="text">
+                        <Label>Line Width</Label>
+                        <Input type="text" autocomplete="off" bind:value={customThemeJSON.daySeparatorLineWidth} />
+                    </label>
+                    <div class="bar"></div>
+                    <label class="text">
+                        <Label>Line Style</Label>
+                        <Input type="text" autocomplete="off" bind:value={customThemeJSON.daySeparatorLineStyle} />
+                    </label>
+                    <div class="bar"></div>
+                    <label class="colorPicker">
+                        <Label>Line Color</Label>
+                        <input type="color" bind:value={customThemeJSON.daySeparatorLineColor}>
+                    </label>
+                </div>
+            </Accordion.Content>
+        </Accordion.Item>
+        <Accordion.Item value="eventstyles">
+            <Accordion.Trigger style="{theme !== "CUSTOM" ? 'opacity: 0.5;':''}">Event Styles</Accordion.Trigger>
+            <Accordion.Content>
+                <div class="customisers">
+                    <label class="colorPicker">
+                        <Label>Background Color</Label>
+                        <input type="color" bind:value={customThemeJSON.eventBackgroundColor}>
+                    </label>
+                    <div class="bar"></div>
+                    <label class="text">
+                        <Label>Border Radius</Label>
+                        <Input type="text" autocomplete="off" bind:value={customThemeJSON.eventBorderRadius} />
+                    </label>
+                    <div class="bar"></div>
+                    <label class="colorPicker">
+                        <Label>Title Color</Label>
+                        <input type="color" bind:value={customThemeJSON.eventTitleColor}>
+                    </label>
+                    <div class="bar"></div>
+                    <label class="colorPicker">
+                        <Label>Resource Text Color</Label>
+                        <input type="color" bind:value={customThemeJSON.resourceTextColor}>
+                    </label>
+                    <div class="bar"></div>
+                    <label class="text">
+                        <Label>Resource Font Size</Label>
+                        <Input type="text" autocomplete="off" bind:value={customThemeJSON.resourceFontSize} />
+                    </label>
+                    <div class="bar"></div>
+                    <label class="text">
+                        <Label>Title Font Size</Label>
+                        <Input type="text" autocomplete="off" bind:value={customThemeJSON.eventTitleFontSize} />
+                    </label>
+                    <div class="bar"></div>
+                    <label class="text">
+                        <Label>Tag Font Size</Label>
+                        <Input type="text" autocomplete="off" bind:value={customThemeJSON.tagFontSize} />
+                    </label>
+                    <div class="bar"></div>
+                    <label class="colorPicker">
+                        <Label>Time Text Color</Label>
+                        <input type="color" bind:value={customThemeJSON.timeTextColor}>
+                    </label>
+                    <div class="bar"></div>
+                    <label class="text">
+                        <Label>Time Text Font Size</Label>
+                        <Input type="text" autocomplete="off" bind:value={customThemeJSON.timeTextFontSize} />
+                    </label>
+                </div>
+            </Accordion.Content>
+        </Accordion.Item>
+    </Accordion.Root>
 {/if}
-<Accordion.Root class="w-full" disabled={theme !== "CUSTOM"}>
-    <Accordion.Item value="mainpagestyles">
-        <Accordion.Trigger style="{theme !== "CUSTOM" ? 'opacity: 0.5;':''}">Main Page Styles</Accordion.Trigger>
-        <Accordion.Content>
-            <div class="customisers">
-                <label class="colorPicker">
-                    <Label>Background Color</Label>
-                    <input type="color" bind:value={customThemeJSON.backgroundColor}>
-                </label>
-            </div>
-        </Accordion.Content>
-    </Accordion.Item>
-    <Accordion.Item value="headerstyles">
-        <Accordion.Trigger style="{theme !== "CUSTOM" ? 'opacity: 0.5;':''}">Header Styles</Accordion.Trigger>
-        <Accordion.Content>
-            <div class="customisers">
-                <label class="colorPicker">
-                    <Label>Header Background Color</Label>
-                    <input type="color" bind:value={customThemeJSON.headerColor}>
-                </label>
-                <div class="bar"></div>
-                <label class="colorPicker">
-                    <Label>Day Text Color</Label>
-                    <input type="color" bind:value={customThemeJSON.dayTextColor}>
-                </label>
-                <div class="bar"></div>
-                <label class="colorPicker">
-                    <Label>Selected Day Text Color</Label>
-                    <input type="color" bind:value={customThemeJSON.selectedDayTextColor}>
-                </label>
-                <div class="bar"></div>
-                <label class="text">
-                    <Label>Day of the Week Text Size</Label>
-                    <Input type="text" autocomplete="off" bind:value={customThemeJSON.dayOfWeekSize} />
-                </label>
-                <div class="bar"></div>
-                <label class="text">
-                    <Label>Header Height</Label>
-                    <Input type="text" autocomplete="off" bind:value={customThemeJSON.headerHeight} />
-                </label>
-                <div class="bar"></div>
-                <label class="text">
-                    <Label>Header Shadow</Label>
-                    <Input type="text" autocomplete="off" bind:value={customThemeJSON.headerBoxShadow} />
-                </label>
-            </div>
-        </Accordion.Content>
-    </Accordion.Item>
-    <Accordion.Item value="dayseparatorstyles">
-        <Accordion.Trigger style="{theme !== "CUSTOM" ? 'opacity: 0.5;':''}">Day Separator Styles</Accordion.Trigger>
-        <Accordion.Content>
-            <div class="customisers">
-                <label class="text">
-                    <Label>Line Width</Label>
-                    <Input type="text" autocomplete="off" bind:value={customThemeJSON.daySeparatorLineWidth} />
-                </label>
-                <div class="bar"></div>
-                <label class="text">
-                    <Label>Line Style</Label>
-                    <Input type="text" autocomplete="off" bind:value={customThemeJSON.daySeparatorLineStyle} />
-                </label>
-                <div class="bar"></div>
-                <label class="colorPicker">
-                    <Label>Line Color</Label>
-                    <input type="color" bind:value={customThemeJSON.daySeparatorLineColor}>
-                </label>
-            </div>
-        </Accordion.Content>
-    </Accordion.Item>
-    <Accordion.Item value="eventstyles">
-        <Accordion.Trigger style="{theme !== "CUSTOM" ? 'opacity: 0.5;':''}">Event Styles</Accordion.Trigger>
-        <Accordion.Content>
-            <div class="customisers">
-                <label class="colorPicker">
-                    <Label>Background Color</Label>
-                    <input type="color" bind:value={customThemeJSON.eventBackgroundColor}>
-                </label>
-                <div class="bar"></div>
-                <label class="text">
-                    <Label>Border Radius</Label>
-                    <Input type="text" autocomplete="off" bind:value={customThemeJSON.eventBorderRadius} />
-                </label>
-                <div class="bar"></div>
-                <label class="colorPicker">
-                    <Label>Title Color</Label>
-                    <input type="color" bind:value={customThemeJSON.eventTitleColor}>
-                </label>
-                <div class="bar"></div>
-                <label class="colorPicker">
-                    <Label>Resource Text Color</Label>
-                    <input type="color" bind:value={customThemeJSON.resourceTextColor}>
-                </label>
-                <div class="bar"></div>
-                <label class="text">
-                    <Label>Resource Font Size</Label>
-                    <Input type="text" autocomplete="off" bind:value={customThemeJSON.resourceFontSize} />
-                </label>
-                <div class="bar"></div>
-                <label class="text">
-                    <Label>Title Font Size</Label>
-                    <Input type="text" autocomplete="off" bind:value={customThemeJSON.eventTitleFontSize} />
-                </label>
-                <div class="bar"></div>
-                <label class="text">
-                    <Label>Tag Font Size</Label>
-                    <Input type="text" autocomplete="off" bind:value={customThemeJSON.tagFontSize} />
-                </label>
-                <div class="bar"></div>
-                <label class="colorPicker">
-                    <Label>Time Text Color</Label>
-                    <input type="color" bind:value={customThemeJSON.timeTextColor}>
-                </label>
-                <div class="bar"></div>
-                <label class="text">
-                    <Label>Time Text Font Size</Label>
-                    <Input type="text" autocomplete="off" bind:value={customThemeJSON.timeTextFontSize} />
-                </label>
-            </div>
-        </Accordion.Content>
-    </Accordion.Item>
-</Accordion.Root>
 <input type="hidden" name="customStyle" form="updateCalendar" bind:value={customThemeText}>
 
 <style>
     .tabs {
-        background-color:#f4f4f5;
+        /* background-color:#f4f4f5; */
         display: grid;
         grid-template-columns: 1fr 1fr 1fr;
         border-radius:5px;
